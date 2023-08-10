456a6431e8076c08a6bdfc0ca2f750fca7c90520
Iraida <ira.serq.1996@gmail.com>
2023-08-10 13:49:12 +0300

    готовый макет

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import * as flsFunctions from "./modules/functions.js";
-import * as menuFunctions from "./modules/burger-menu.js";
-import * as valueFunctions from "./modules/range-value.js";
-import * as stickyFunctions from "./modules/sticky-header.js";
-
-flsFunctions.isWebp();
-menuFunctions.burgerMenu();
-valueFunctions.value();
-stickyFunctions.stickyHeader();
